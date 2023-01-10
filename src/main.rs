2bd94d082f39c491ba3a0e7b8b799f4fb53a125f
Kenny Sweet <redredacted@gmail.com>
2023-01-09 21:18:37 -0600

    - cargo init

---
+++ src/main.rs
@@ -0,0 +1,16 @@
+use axum::{
+    routing::get,
+    Router,
+};
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new()
+        .route("/", get(|| async { "Hello, World!" }))
+        .route("/foo", get(|| async { "Hello, Foo!" }));
+    
+    axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
